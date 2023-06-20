ffda17b2888fe99116c167305bef36a54181f836
Rajni bala <balatamna16@gmail.com>
2023-06-20 16:18:10 -0400

    license badge

--- Develop/index.js
+++ Develop/index.js
@@ -3,6 +3,35 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 const generateMarkdown = require("./utils/generateMarkdown.js");
+
+//Licensebadge function starts here
+function licenseOptions(value){
+  if(value === "Apache 2.0 License"){
+    return"[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  }
+  else if(value === "BSD 3-Clause License" ){
+    return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+  }
+  else if(value === "GNU GPL v3"){
+return "[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  }
+  else if(value === "IBM Public License Version 1.0"){
+    return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+}
+else if(value === "MIT"){
+  return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+}
+else if(value === "Mozilla Public License 2.0"){
+  return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+}
+else{
+  return"[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)";
+}
+
+
+
+}
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
